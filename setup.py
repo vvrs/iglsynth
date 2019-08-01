b7dc2ec6c2536c7b6995927d4ac273d2aa26d7f6
Abhishek Kulkarni <abhi.bp1993@gmail.com>
2019-08-01 14:49:53 -0400

    setup.py created

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='iglsynth',
+    packages=find_packages(),
+    version='0.1.1',
+    description='Infinite Games on graph and Logic-based controller Synthesis',
+    author='Abhishek N. Kulkarni',
+    author_email='ankulkarni@wpi.edu',
+    url='https://github.com/abhibp1993/iglsynth',
+    install_requires=['pytest'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English", "Operating System :: Ubuntu",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ])
